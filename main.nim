3e1c341d8abed8542865c42a01b49148aae5eaed
aguspiza <aguspiza@yahoo.es>
2019-07-27 18:23:37 +0200

    chapter 5

---
+++ main.nim
@@ -0,0 +1,46 @@
+import os, strformat, math, v3
+
+
+let rows : int = 400
+let cols : int = 800
+
+let scrOrigin = Vec3(x: -2f, y: -1f, z: -1f)
+let hor = Vec3(x: 4f, y: 0f, z: 0f)
+let vert = Vec3(x: 0.0f, y: 2f, z: 0f)
+let origin = Vec3(x: 0f, y: 0f, z: 0f)
+
+proc color (ray: Ray) : Vec3 =
+    if ray.hit(Sphere(o: Vec3(x: 0f, y: 0f, z: -1f), r: 0.5f)):
+        return Vec3(x: 1f, y:0f, z:0f)
+    let unitV = ray.b.normalize()
+    let t = 0.5f32 * (unitV.y + 1f)
+    return (vec3Unit()*(1.0f - t)) + ((Vec3(x: 0.5f, y: 0.7f, z: 1f)*t))
+
+var lines = newSeq[string]()
+#header
+lines.add "P3"
+lines.add fmt"{cols} {rows}"
+lines.add "255"
+
+#pixels
+for j in countdown(rows-1,0):
+    for i in 0 ..< cols:
+        let vec = Vec3(x: float32(i) / float32(cols), y: float32(j) / float32(rows), z: 0.2f32)
+        let ray = Ray(a: origin, b: scrOrigin + (hor * vec.x) + (vert * vec.y))
+        let col = ray.color()
+        let ir = uint8(255.99f32 * col.x)
+        let ig = uint8(255.99f32 * col.y)
+        let ib = uint8(255.99f32 * col.z)
+        lines.add fmt"{ir} {ig} {ib}"
+
+#write file
+let file = open("ray.ppm", FileMode.fmReadWrite)
+for line in lines:
+    file.writeLine(line)
+file.flushFile()
+file.close()
+
+let v1 = Vec3(x: 0f, y: 1f, z:2f)
+let v2 = Vec3(x: 0f, y: 1f, z:2f)
+echo v1.dot(v2)
+echo v1.cross(v2)
---
+++ v3.nim
@@ -0,0 +1,53 @@
+import math
+
+type
+    Vec3* = object
+        x*, y*, z*: float32
+    Ray* = object
+        a*, b*: Vec3
+    Sphere* = object
+        o*: Vec3
+        r*: float32
+
+template `+`*(v1: Vec3, v2: Vec3): Vec3 =
+    Vec3(x: v1.x + v2.x, y: v1.y + v2.y, z: v1.z + v2.z)
+
+template `-`*(v1: Vec3): Vec3 =
+    Vec3(x: -v1.x, y: -v1.y, z: -v1.z)
+
+template `+`*(v1: Vec3, f: float32): Vec3 =
+    Vec3(x: v1.x + f, y: v1.y + f, z: v1.z + f)
+
+template `*`*(v1: Vec3, f: float32): Vec3 =
+    Vec3(x: v1.x * f, y: v1.y * f, z: v1.z * f)
+
+template dot*(v1: Vec3, v2: Vec3 ) : float32 =
+    v1.x * v2.x + v1.y * v2.y + v1.z * v2.z
+
+template cross*(v1: Vec3, v2: Vec3 ) : Vec3 =
+    Vec3(x: v1.y * v2.z - v1.z * v2.y, y: -v1.x * v2.z - v1.z * v2.x, z: v1.x * v2.y - v1.y * v2.x )
+
+template sqlen*(vec: Vec3) : float32 =
+    vec.x * vec.x + vec.y * vec.y + vec.z * vec.z
+
+template len*(vec: Vec3) : float32 =
+    sqrt(vec.sqlen)
+
+template pointAt*(ray: Ray, t: float32) : Vec3 =
+    Vec3(x: ray.a.x + t*ray.b.x, y: ray.a.y + t*ray.b.y, z: ray.a.z + t*ray.b.z)
+
+proc normalize*(vec: Vec3) : Vec3 =
+    let k = 1f32 / (vec.len+0.000001f)
+    result = Vec3(x: vec.x * k, y: vec.y * k, z: vec.z * k)
+    assert result.len <= 1.0f
+    assert result.len > 0.99f
+
+proc vec3Unit*() : Vec3 = Vec3(x: 1f, y: 1f, z: 1f)
+
+proc hit*(ray: Ray, s: Sphere) : bool =
+    let oc = ray.a + -s.o
+    let a = ray.b.dot(ray.b)
+    let b = oc.dot(ray.b) * 2f
+    let c = oc.dot(oc) - s.r*s.r
+    let res = b*b - 4*a*c
+    result = res > 0
