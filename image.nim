23e33fd1f3d02acd4887513f447ed31b5aae9bba
aguspiza <aguspiza@yahoo.es>
2019-07-28 16:13:18 +0200

    targa format and move scrOrigin to top-left

---
+++ image.nim
@@ -0,0 +1,77 @@
+import streams, strformat, algorithm
+
+type
+    Rgb* = tuple[r: uint8, g: uint8, b: uint8]
+    TargaImg = object
+        header: array[18, uint8]
+        pixels: seq[Rgb]
+    PpmImg = object
+        lines: seq[string]
+    Writeable* = concept w
+        var s: Stream
+        s.write(w)
+
+template writeTo*(w: Writeable, fileName: string) =
+    let file = newFileStream(fileName, FileMode.fmReadWrite)
+    file.write(w)
+    file.flush()
+    file.close()
+
+#TOOD: access rgb data directly
+template toOpenArray*(rgb: Rgb) : openArray[uint8] = toOpenArray([rgb[0], rgb[1], rgb[2]], 0, 2)
+
+iterator ritems*[T](a: seq[T]): T {.inline.} =
+  ## Iterates over each item of `a` backwards
+  var i = len(a)
+  let L = len(a)
+  while i > 0:
+    dec(i)
+    yield a[i]
+    assert(len(a) == L, "the length of the seq changed while iterating over it")
+
+proc clear(seq: var seq[Rgb]) =
+    seq.setLen(0)
+
+proc newTarga*(width: int32, height: int32, pixels: seq[Rgb]) : TargaImg =
+    var tga = TargaImg()
+    tga.header[2] = 2
+    tga.header[12] = uint8(255 and width)
+    tga.header[13] = uint8(255 and (width shr 8))
+    tga.header[14] = uint8(255 and height)
+    tga.header[15] = uint8(255 and (height shr 8))
+    tga.header[16] = 24u8
+    #tga.header[17] = 32u8
+
+    var row = newSeq[Rgb]()
+    #from botton to up
+    for pixel in pixels.ritems:
+        row.add(pixel)
+        if row.len < width:
+            continue
+        #"unreverse" row
+        for pixel2 in row.ritems:
+            tga.pixels.add pixel2
+        row.clear()
+    return tga
+
+proc newPpm*(width: int32, height: int32, pixels: seq[Rgb]) : PpmImg =
+    var ppm = PpmImg()
+    ppm.lines.add "P3"
+    ppm.lines.add fmt"{width} {height}"
+    ppm.lines.add "255"
+
+    for pixel in pixels:
+        ppm.lines.add fmt"{pixel[0]} {pixel[1]} {pixel[2]}"
+    return ppm
+
+proc write*(strm: Stream, tga: TargaImg) =
+    strm.write(tga.header)
+    #targa is BGR not RGB
+    for pixel in tga.pixels:
+        strm.write(pixel.b)
+        strm.write(pixel.g)
+        strm.write(pixel.r)
+
+proc write*(strm: Stream, ppm: PpmImg) =
+    for line in ppm.lines:
+        strm.writeLine(line)
--- main.nim
+++ main.nim
@@ -1,13 +1,16 @@
-import os, strformat, math, v3
+import os, streams, strformat, math, v3, image
 
+let rows : int32 = 400
+let cols : int32 = 800
 
-let rows : int = 400
-let cols : int = 800
-
-let scrOrigin = Vec3(x: -2f, y: -1f, z: -1f)
+#top left
+let scrOrigin = Vec3(x: -2f, y: 1f, z: -1f)
 let hor = Vec3(x: 4f, y: 0f, z: 0f)
-let vert = Vec3(x: 0.0f, y: 2f, z: 0f)
+let vert = Vec3(x: 0.0f, y: -2f, z: 0f)
 let origin = Vec3(x: 0f, y: 0f, z: 0f)
+
+template toRgb(v: Vec3) : Rgb = 
+    (r: uint8(255.99f32 * v.x), g: uint8(255.99f32 * v.y), b: uint8(255.99f32 * v.z) )
 
 proc color (ray: Ray) : Vec3 =
     let sphere = Sphere(o: Vec3(x: 0f, y: 0f, z: -1f), r: 0.5f)
@@ -16,32 +19,22 @@ proc color (ray: Ray) : Vec3 =
         let u = normalize(ray.pointAt(hitted) + -Vec3(x: 0f, y: 0f, z: -1f))
         return Vec3(x: u.x + 1f, y: u.y + 1f, z: u.z + 1f)*0.5
     let unitV = ray.b.normalize()
-    let t = 0.5f32 * (unitV.y + 1f)
+    let t = 0.5f * (unitV.y + 1f)
     return (vec3Unit()*(1.0f - t)) + ((Vec3(x: 0.5f, y: 0.7f, z: 1f)*t))
 
-var lines = newSeq[string]()
-#header
-lines.add "P3"
-lines.add fmt"{cols} {rows}"
-lines.add "255"
-
-#pixels
-for j in countdown(rows-1,0):
+var pixels = newSeq[Rgb]()
+for j in 0 ..< rows:
     for i in 0 ..< cols:
         let vec = Vec3(x: float32(i) / float32(cols), y: float32(j) / float32(rows), z: 0.2f32)
         let ray = Ray(a: origin, b: scrOrigin + (hor * vec.x) + (vert * vec.y))
-        let col = ray.color()
-        let ir = uint8(255.99f32 * col.x)
-        let ig = uint8(255.99f32 * col.y)
-        let ib = uint8(255.99f32 * col.z)
-        lines.add fmt"{ir} {ig} {ib}"
+        let col = ray.color().toRgb()
+        pixels.add col
 
 #write file
-let file = open("ray.ppm", FileMode.fmReadWrite)
-for line in lines:
-    file.writeLine(line)
-file.flushFile()
-file.close()
+let tga = newTarga(cols, rows, pixels)
+tga.writeTo("ray.tga")
+let ppm = newPpm(cols, rows, pixels)
+ppm.writeTo("ray.ppm")
 
 when defined(tests):
     let v1 = Vec3(x: 0f, y: 1f, z:2f)
@@ -49,3 +42,7 @@ when defined(tests):
     echo v1.dot(v2)
     echo v1.cross(v2)
     echo -vec3Unit()
+    echo (1u8, 2u8, 3u8).toOpenArray()
+    let b = @[ (0u8,0u8,0u8), (255u8,0u8,255u8), (0u8,255u8,255u8), (255u8,0u8,0u8), (0u8,255u8,0u8), (0u8,0u8,255u8) ]
+    newTarga(3, 2, b).writeTo("test.tga")
+    newPpm(3, 2, b).writeTo("test.ppm")
